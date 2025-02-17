b4e6bad58e7fcfa661b51d191eaab3058be09a5c
peter-francois <contact@peterfrancois.dev>
2025-02-17 11:47:29 +0100

    update: cleaning views.py and adding the expiration_date in add_to_cart

--- web_shop/page_vente/views.py
+++ web_shop/page_vente/views.py
@@ -5,6 +5,8 @@ from .forms import ProductFilterForm
 from django.core.paginator import Paginator
 from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_protect
+from django.utils.timezone import now
+from .utils import get_session_expiration
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(AllProducts, id=product_id)
@@ -13,11 +15,14 @@ def add_to_cart(request, product_id):
         return JsonResponse({'success': False, 'message': 'Produit déjà pris'}, status=400)
 
     # Récupérer l'ID de session unique de Django
+    expiration_date = get_session_expiration(request)
     session_id = request.session.session_key
+    
     if not session_id:
         request.session.create()
         session_id = request.session.session_key
 
+    
     # Vérifier si un panier existe pour cette session
     cart, created = Cart.objects.get_or_create(session_id=session_id)
 
@@ -31,7 +36,7 @@ def add_to_cart(request, product_id):
     product.disponible = False
     product.save()
 
-    return JsonResponse({'success': True, 'message': f'{product.nom} ajouté au panier'})
+    return JsonResponse({'success': True, 'message': f'{product.nom} ajouté au panier', "session_expiration": expiration_date.strftime('%Y-%m-%d %H:%M:%S') if expiration_date else None})
 
 def cart_detail(request):
     session_id = request.session.session_key
@@ -120,29 +125,6 @@ def rendre_disponible(request, product_id):
 def index(request):    
     return render(request, 'page_vente/index.html')
 
-
-    """macrames_objects = Macrame.objects.all()
-    macrames_disponible = [macrame.nom for macrame in macrames_objects if macrame.disponible == True]
-    maroquinerie_objects = Maroquinerie.objects.all()
-    maroquinerie_disponible = [maroquinerie.nom for maroquinerie in maroquinerie_objects if maroquinerie.disponible == True]
-    together_objects = Together.objects.all()
-    together_disponible = [together.nom for together in together_objects if together.disponible == True]
-    liste_objects_disponible = []
-    if macrames_disponible:
-        liste_objects_disponible.append("Marcramés disponible")
-    else:
-        liste_objects_disponible.append("Macramés indisponible")
-    if maroquinerie_disponible:
-        liste_objects_disponible.append("Maroquinerie disponible")
-    else:
-        liste_objects_disponible.append("Maroquinerie indisponible")
-    if together_disponible:
-        liste_objects_disponible.append("Together disponible")
-    else:
-        liste_objects_disponible.append("Together indisponible")
-    return HttpResponse("Liste Objects disponible: " + str(liste_objects_disponible))"""
-
-
 def macrames(request):
     return render(request, 'page_vente/macrames.html')
 
@@ -214,76 +196,16 @@ def contact(request):
     return render(request, 'page_vente/contact.html')
 
 def panier(request):
-    return render(request, 'page_vente/panier.html')
+    session_key = request.session.session_key
+    cart = Cart.objects.filter(session_id=session_key).first()
+    items = CartItem.objects.filter(cart=cart)
+    expiration_date = get_session_expiration(request)
+
+
+    return render(request, "page_vente/panier.html", {
+        "expiration_date": expiration_date,
+        "items": items
+    })
 
 def a_propos(request):
     return render(request, 'page_vente/a_propos.html')
-
-
-    """
-    macrames_objects = Macrame.objects.all()
-
-    # Collier
-    macrames_colliers_names = [macrame.nom for macrame in macrames_objects if macrame.type == "Collier" and
-                               macrame.disponible == True]
-    macrames_colliers_names_str = ""
-    if macrames_colliers_names:
-        if len(macrames_colliers_names) > 1:
-            macrames_colliers_names_str = ", ".join(macrames_colliers_names)
-        else:
-            macrames_colliers_names_str = macrames_colliers_names[0]
-
-    # Bracelet
-    macrames_bracelet_names = [macrame.nom for macrame in macrames_objects if macrame.type == "Bracelet" and
-                               macrame.disponible == True]
-    macrames_bracelets_names_str = ""
-    if macrames_bracelet_names:
-        if len(macrames_bracelet_names) > 1:
-            macrames_bracelets_names_str = ", ".join(macrames_bracelet_names)
-        else:
-            macrames_bracelets_names_str = macrames_bracelet_names[0]
-
-    # Murale
-    macrames_murales_names = [macrame.nom for macrame in macrames_objects if macrame.type == "Murale" and
-                              macrame.disponible == True]
-    macrames_murales_names_str = ""
-    if macrames_murales_names:
-        if len(macrames_murales_names) > 1:
-            macrames_murales_names_str = ", ".join(macrames_murales_names)
-        else:
-            macrames_murales_names_str = macrames_murales_names[0]
-
-    # Boucles d'oreilles
-    macrames_boucles_d_oreilles_names = [macrame.nom for macrame in macrames_objects if
-                                         macrame.type == "Boucles d'oreilles" and
-                                         macrame.disponible == True]
-    macrames_boucles_d_oreilles_names_str = ""
-    if macrames_boucles_d_oreilles_names:
-        if len(macrames_boucles_d_oreilles_names) > 1:
-            macrames_boucles_d_oreilles_names_str = ", ".join(macrames_boucles_d_oreilles_names)
-        else:
-            macrames_boucles_d_oreilles_names_str = macrames_boucles_d_oreilles_names[0]
-
-    # Divers
-    macrames_divers_names = [macrame.nom for macrame in macrames_objects if macrame.type == "Diver" and
-                             macrame.disponible == True]
-    macrames_divers_names_str = ""
-    if macrames_divers_names:
-        if len(macrames_divers_names) > 1:
-            macrames_divers_names_str = ", ".join(macrames_divers_names)
-        else:
-            macrames_divers_names_str = macrames_divers_names[0]
-    list_macrames_complette = ""
-    if any([macrames_colliers_names_str,
-            macrames_bracelets_names_str,
-            macrames_boucles_d_oreilles_names_str,
-            macrames_divers_names_str]):
-        list_macrames_complette = f  Les colliers: {macrames_colliers_names_str}<br>
-                                        Les bracelets: {macrames_bracelets_names_str}<br>
-                                        Les murales: {macrames_murales_names_str}<br>
-                                        Les boucles d'oreilles: {macrames_boucles_d_oreilles_names_str}<br>
-                                        Les divers: {macrames_divers_names_str}
-    else:
-        list_macrames_complette = "Désolé, il n'y a pas de macrames_objects en ce moment"
-    return HttpResponse("Les macramés:<br>" + list_macrames_complette)
-    """
