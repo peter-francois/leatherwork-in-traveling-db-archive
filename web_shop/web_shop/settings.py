3776069e1ba8205e661e53212110aa04a87d44df
peter-francois <contact@peterfrancois.dev>
2025-02-21 09:26:18 +0100

    feat: adding staticfiles for be able to use static in pythonanywhere

--- web_shop/web_shop/settings.py
+++ web_shop/web_shop/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Dossier où Django va collecter les fichiers statiques
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'page_vente/static'),  # Dossier où tu mets tes fichiers statiques
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
