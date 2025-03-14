0e79a9c0280fe718a8765195e5562b6c7ed4a713
peter-francois <contact@peterfrancois.dev>
2025-03-14 18:35:38 +0100

    feat: creat script for reset to none cgv data that are older than 5 years

---
+++ web_shop/page_vente/management/commands/release_expiring_CGV_Acceptation.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from page_vente.models import Cart
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = "Libère les acceptations des CGV après 5 ans"
+
+    def handle(self, *args, **kwargs):
+        # Seuil de libération des cgv
+        expiration_date = timezone.now() - timedelta(days=5*365)
+
+        # Trouver les paniers dont les CGV ont expiré
+        expired_cgv_cart = Cart.objects.filter(cgv_expires_at__lt=expiration_date)
+
+        if not expired_cgv_cart.exists():
+            self.stdout.write("✅ Aucun CGV expiré à libérer.")
+            return
+
+        # Réinitialiser les preuves d'acceptation des CGV pour les paniers expirés
+        for cart in expired_cgv_cart:
+            cart.cgv_accepted = None  # Supprimer la référence à CGV
+            cart.cgv_accepted_at = None  # Réinitialiser la date d'acceptation
+            cart.cgv_expires_at = None  # Réinitialiser la date d'expiration
+            cart.save()
+
+        self.stdout.write(f"✔️ Preuves d'acceptation des CGV supprimées pour {expired_cgv_cart.count()} panier(s).")
