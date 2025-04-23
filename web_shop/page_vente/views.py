13b295f57f122b1af16159c655a5fb17e80afb61
peter-francois <contact@peterfrancois.dev>
2025-04-23 11:54:00 +0200

    fix(cache): keep url from cloudinary for img in modal for keep good quality

--- web_shop/page_vente/views.py
+++ web_shop/page_vente/views.py
@@ -252,16 +252,14 @@ def remove_from_cart(request, product_id):
 def get_product_images(request, article_id):
     try:
         product = AllProducts.objects.get(id=article_id)
-        images = get_cached_images_for_product(product)
-        # On retourne les images sous forme de liste (comme avant)
-        images_list = [images['image1'], images['image2'], images['image3'], images['image4']]
-        images_list = [img for img in images_list if img]  # Filtrer les vides
-        return JsonResponse({
-            'images': images_list,
-            'nom': product.nom,
-            'description': product.description if product.description else None,
-            'prix': product.prix
-        })
+        images = [
+            product.image1.url if product.image1 else None,
+            product.image2.url if product.image2 else None,
+            product.image3.url if product.image3 else None,
+            product.image4.url if product.image4 else None,
+            ]
+        images = [image for image in images if image]
+        return JsonResponse({'images': images, 'nom': product.nom, 'description': product.description if product.description else None, 'prix': product.prix})
     except AllProducts.DoesNotExist:
         return JsonResponse({'error': 'Product not found'}, status=404)
 
