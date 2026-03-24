fa510a45b4eb27533f564563ad8254c4cd778a57
peter-francois <contact@peterfrancois.dev>
2026-03-24 12:08:14 +0100

    test(core): create test for index

--- web_shop/core/tests.py
+++ web_shop/core/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    """Tests for the index/homepage view"""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_returns_200(self):
+        """Should return a 200 status code"""
+        response = self.client.get(reverse('core:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sets_csrf_cookie(self):
+        """Should set a CSRF cookie (required for JS requests)"""
+        response = self.client.get(reverse('core:index'))
+        self.assertIn('csrftoken', response.cookies)
+
+    def test_uses_correct_template(self):
+        """Should render core/index.html"""
+        response = self.client.get(reverse('core:index'))
+        self.assertTemplateUsed(response, 'core/index.html')
