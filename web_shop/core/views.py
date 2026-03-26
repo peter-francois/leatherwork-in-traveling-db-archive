2075a69e9294a9dbae51b052350f454c94edda88
peter-francois <contact@peterfrancois.dev>
2026-03-26 11:36:08 +0100

    refactor(core): create generate_sitemap_index to services and fix sitemap issue

--- web_shop/core/views.py
+++ web_shop/core/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from core.sitemaps import StaticSitemap
 from core.sitemaps_paginated import PaginatedCategorySitemap
 from django.contrib.sitemaps.views import sitemap as django_sitemap
+from .services import generate_sitemap_index
 
 @ensure_csrf_cookie
 def index(request):
@@ -24,17 +25,7 @@ def sitemap_lang(request, lang):
 
 def sitemap_index(request):
     base_url = request.build_absolute_uri('/')
-    sitemap_urls = [
-        f'{base_url}sitemap-fr.xml',
-        f'{base_url}sitemap-en.xml',
-    ]
-
-    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-    for url in sitemap_urls:
-        xml += f'  <sitemap>\n    <loc>{url}</loc>\n  </sitemap>\n'
-    xml += '</sitemapindex>'
-
+    xml = generate_sitemap_index(base_url, ['fr', 'en'])
     return HttpResponse(xml, content_type='application/xml')
 
 def robots_txt(request):
